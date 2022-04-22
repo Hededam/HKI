c5279176374a9882873aa55e6036cd0310ac0df2
Mads Hededam <Hededam@live.dk>
2022-04-22 10:53:08 +0200

    Test commit

---
+++ VigtigHeden/Assets/Scripts/FractureStuff.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractureStuff : MonoBehaviour
+{
+    public GameObject fractured;
+    private Collider Col;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Weapon")
+        {
+            Instantiate(fractured, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
