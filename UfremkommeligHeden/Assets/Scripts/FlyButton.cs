039c0fdb2bcc8cc5ec230280d49f0ef9c47ce261
Mads Hededam <dt_Hededam@hotmail.com>
2022-09-10 23:14:20 +0200

    computerpult i start område

---
+++ UfremkommeligHeden/Assets/Scripts/FlyButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyButton : MonoBehaviour {
+    Camera mainCam;
+
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            print("Space pressed!");
+            mainCam.transform.position = new Vector3(mainCam.transform.position.x, mainCam.transform.position.y, mainCam.transform.position.z + 10f);
+            print(" position : " + mainCam.transform.position);
+        }
+
+    }
+}
