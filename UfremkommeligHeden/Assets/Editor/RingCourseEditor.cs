3e00185d50c5e6cb9b027f6fc65f258c06ce90fd
Mads Hededam <Hededam@live.dk>
2022-12-01 15:11:38 +0100

    har importet edderkobber

--- UfremkommeligHeden/Assets/Editor/RingCourseEditor.cs
+++ UfremkommeligHeden/Assets/Editor/RingCourseEditor.cs
@@ -5,27 +5,24 @@ using UnityEditor;
 using UnityEditor.EditorTools;
 using RingCourse;
 
-//[CustomEditor(typeof(RingCourseManager))]
-[EditorTool("Ring Course Tool", typeof(RingCourseManager))]
-class RingCourseEditor : EditorTool, IDrawSelectedHandles
+[CustomEditor(typeof(RingCourseManager))]
+//[EditorTool("Ring Course Tool", typeof(RingCourseManager))]
+class RingCourseEditor : Editor//, IDrawSelectedHandles
 {
     private RingCourseManager manager;
 
     void OnEnable()
     {
-        manager = target as RingCourseManager;
     }
 
     public void OnSceneGUI()
     {
+        manager = (RingCourseManager)target;
         //foreach (RingBase ring in manager.rings)
         //{
         //    Handles.TransformHandle(ring.transform.position, ring.transform.rotation, ring.transform.localScale);
         //}
-    }
 
-    public void OnDrawHandles()
-    {
         Handles.color = Color.cyan;
         for (int i = 1; i < manager.rings.Length; i++)
         {
@@ -35,7 +32,7 @@ class RingCourseEditor : EditorTool, IDrawSelectedHandles
             float buttonSize = HandleUtility.GetHandleSize(buttonPos) * 0.2f;
 
             Handles.DrawLine(pos1, pos2, 3.0f);
-            if(Handles.Button(buttonPos, SceneView.currentDrawingSceneView.rotation, buttonSize, buttonSize + 0.5f, Handles.RectangleHandleCap))
+            if (Handles.Button(buttonPos, SceneView.currentDrawingSceneView.rotation, buttonSize, buttonSize + 0.5f, Handles.DotHandleCap))
             {
                 Debug.Log("test");
             }
