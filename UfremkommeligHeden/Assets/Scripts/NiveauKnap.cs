c9bbe01c99abbece2c19bf35203f2d089c8e6389
Mads Hededam <Hededam@live.dk>
2023-01-26 14:48:15 +0100

    Niveau funktion i edderkoppeland

--- UfremkommeligHeden/Assets/Scripts/NiveauKnap.cs
+++ UfremkommeligHeden/Assets/Scripts/NiveauKnap.cs
@@ -8,10 +8,22 @@ public class NiveauKnap : MonoBehaviour
     public GameObject Niveau1;
     public GameObject Niveau2;
     public GameObject Niveau3;
+    public GameObject Niveau4;
+    public GameObject Niveau5;
+    public GameObject Niveau6;
     public string noget;
-    public void aktiverX()
+    public void aktiverDown()
     {
-
+        Niveau1.SetActive(false);
+        Niveau2.SetActive(false);
+        Niveau3.SetActive(false);
+        Niveau4.SetActive(false);
+        Niveau5.SetActive(false);
+        Niveau6.SetActive(false);
+    }
+    public void aktiverUP()
+    {
+ 
         target.SetActive(true);
     }
     // Start is called before the first frame update
@@ -20,5 +32,8 @@ public class NiveauKnap : MonoBehaviour
         Niveau1.SetActive(false);
         Niveau2.SetActive(false);
         Niveau3.SetActive(false);
+        Niveau4.SetActive(false);
+        Niveau5.SetActive(false);
+        Niveau6.SetActive(false);
     }
 }
