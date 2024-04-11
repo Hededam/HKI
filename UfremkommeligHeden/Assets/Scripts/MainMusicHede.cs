0d6b879cfd23c3d59a096592e3825d3e1249f462
Mads Hededam <dt_Hededam@hotmail.com>
2024-04-11 14:07:07 +0200

    opdateret musik player-script

--- UfremkommeligHeden/Assets/Scripts/MainMusicHede.cs
+++ UfremkommeligHeden/Assets/Scripts/MainMusicHede.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; // Tilføj denne linje for at bruge UI elementer
+using UnityEngine.UI;
 
 public class MainMusicHede : MonoBehaviour
 {
@@ -11,24 +11,79 @@ public class MainMusicHede : MonoBehaviour
 
     public List<AudioClip> myClips = new List<AudioClip>();
 
-    // Tilføj denne linje for at referere til din knap
     public Button myButton;
+    public Button muteButton;
+    public Button nextButton;
+    public Button unmuteButton;
+    public Button previousButton;
+
+    private int currentClipIndex = 0;
 
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
 
-        // Tilføj denne linje for at tilknytte PlayRandomSound funktionen til knappens OnClick event
-        myButton.onClick.AddListener(() => PlayRandomSound(1.0f)); // Du kan ændre volumen parameteren som du vil
+        myButton.onClick.AddListener(() => PlayRandomSound(1.0f));
+        muteButton.onClick.AddListener(Mute);
+        unmuteButton.onClick.AddListener(Unmute);
+        nextButton.onClick.AddListener(PlayNextSound);
+        previousButton.onClick.AddListener(PlayPreviousSound);
     }
 
-    public void PlayRandomSound(float vol)     //adjust preferred volume of particular clip in "vol" 
+    public void PlayRandomSound(float vol)
     {
-        int randomIndex = Random.Range(0, myClips.Count); // Vælg en tilfældig sang fra listen
+        int randomIndex = Random.Range(0, myClips.Count);
         AudioClip myClip = myClips[randomIndex];
 
         soundSource.clip = myClip;
         soundSource.volume = masterSoundVolume * vol;
         soundSource.Play();
+
+        StartCoroutine(PlayNextClipAfterDelay(myClip.length)); // Tilføj denne linje
+    }
+
+    public void Mute()
+    {
+        soundSource.mute = true;
+    }
+
+    public void Unmute()
+    {
+        soundSource.mute = false;
+    }
+
+    public void PlayNextSound()
+    {
+        currentClipIndex = (currentClipIndex + 1) % myClips.Count;
+        AudioClip myClip = myClips[currentClipIndex];
+
+        soundSource.clip = myClip;
+        soundSource.volume = masterSoundVolume;
+        soundSource.Play();
+
+        StartCoroutine(PlayNextClipAfterDelay(myClip.length)); // Tilføj denne linje
+    }
+
+    public void PlayPreviousSound()
+    {
+        currentClipIndex--;
+        if (currentClipIndex < 0)
+        {
+            currentClipIndex = myClips.Count - 1;
+        }
+        AudioClip myClip = myClips[currentClipIndex];
+
+        soundSource.clip = myClip;
+        soundSource.volume = masterSoundVolume;
+        soundSource.Play();
+
+        StartCoroutine(PlayNextClipAfterDelay(myClip.length)); // Tilføj denne linje
+    }
+
+    // Tilføj denne coroutine for at afspille det næste klip efter en forsinkelse
+    IEnumerator PlayNextClipAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        PlayNextSound();
     }
 }
