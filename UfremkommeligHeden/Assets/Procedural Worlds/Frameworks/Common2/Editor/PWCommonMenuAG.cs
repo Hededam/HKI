22db4e39dade5fe2ef5530b3209c6b778cfe6422
Mads Hededam <dt_Hededam@hotmail.com>
2024-12-11 22:59:45 +0100

    unity 6000.  26

--- UfremkommeligHeden/Assets/Procedural Worlds/Frameworks/Common2/Editor/PWCommonMenuAG.cs
+++ UfremkommeligHeden/Assets/Procedural Worlds/Frameworks/Common2/Editor/PWCommonMenuAG.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright © 2018 Procedural Worlds Pty Limited.  All Rights Reserved.
 using UnityEngine;
 using UnityEditor;
-using PWCommon5;
+using PWCommon2;
 
-namespace PWCommon5.Internal
+namespace PWCommon2.Internal
 {
     public class CommonMenu : Editor
     {
